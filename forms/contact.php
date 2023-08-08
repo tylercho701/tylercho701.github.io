c6ce3db5a9373bd3c47dedfad22d3acf38706bd1
tylercho701 <tylercho701@gmail.com>
2023-08-08 10:10:15 +0900

    to hide imap info onto contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -7,7 +7,7 @@
   */
 
   // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'tylercho701@gmail.com';
+  $receiving_email_address = 'example';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
@@ -24,14 +24,14 @@
   $contact->subject = $_POST['subject'];
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
- 
+ /*
   $contact->smtp = array(
-    'host' => 'smtp.gmail.com',
-    'username' => 'tylercho701@gmail.com',
-    'password' => 'mcmsclyeblcctijq',
-    'port' => '587'
+    'host' => host,
+    'username' => username,
+    'password' => userpass,
+    'port' => port
   );
- 
+ */
 
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
